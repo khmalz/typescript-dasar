8659e672a1156da7ee7fabdfc072814488d841c9
khmalz <akmalrma1710@gmail.com>
2023-07-07 08:56:06 +0700

    Function Parameter

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -12,4 +12,30 @@ describe("Function", () => {
 
       printHello("Akmal");
    });
+
+   it("should support default value", () => {
+      const sayHello = (name: string = "Guest"): string => {
+         return `Hello ${name}`;
+      };
+
+      expect(sayHello("Akmal")).toBe("Hello Akmal");
+      expect(sayHello()).toBe("Hello Guest");
+   });
+
+   it("should support rest parameter", () => {
+      const sum = (...values: number[]): number => {
+         return values.reduce((acc, sum) => acc + sum, 0);
+      };
+
+      expect(sum(1, 2, 3, 4, 5)).toBe(15);
+   });
+
+   it("should support optional parameter", () => {
+      const sayHello = (firstName: string, lastName?: string): string => {
+         return lastName ? `Hello ${firstName} ${lastName}` : `Hello ${firstName}`;
+      };
+
+      expect(sayHello("Khairul")).toBe("Hello Khairul");
+      expect(sayHello("Khairul", "Akmal")).toBe("Hello Khairul Akmal");
+   });
 });
